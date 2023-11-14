29bcfd924e9399d6126905725b28f7db9bed665d
adii1203 <aditya32ft@gmail.com>
2023-11-14 23:13:25 +0530

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {" "}
-      <div className="">
-        <Login />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
