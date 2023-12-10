fdd94731102e8b545cb6a7d7c9dd5472cf8f60b0
adii1203 <aditya32ft@gmail.com>
2023-12-10 14:38:51 +0530

    style: ui changes in AllLinks component.

--- src/components/AllLinks.jsx
+++ src/components/AllLinks.jsx
@@ -1,55 +1,11 @@
 /* eslint-disable react/prop-types */
-import { Clipboard, Pencil, QrCode } from "lucide-react";
-
+import { BarChart, Clipboard, QrCode } from "lucide-react";
+import { useSelector } from "react-redux";
 const AllLinks = () => {
-  const links = [
-    {
-      shortLink: "short.sh/A34dksf",
-      originalLink: "https://www.youtube.com/watch",
-      clicks: 123,
-      status: "active",
-    },
-    {
-      shortLink: "short.sh/A34dksf",
-      originalLink: "https://www.youtube.com/watch",
-      clicks: 123,
-      status: "active",
-    },
-    {
-      shortLink: "short.sh/A34dksf",
-      originalLink: "https://www.youtube.com/watch",
-      clicks: 123,
-      status: "active",
-    },
-    {
-      shortLink: "short.sh/A34dksf",
-      originalLink: "https://www.youtube.com/watch",
-      clicks: 245,
-      status: "deactive",
-    },
-    {
-      shortLink: "short.sh/A34dksf",
-      originalLink: "https://www.youtube.com/watch",
-      clicks: 98,
-      status: "active",
-    },
-    {
-      shortLink: "short.sh/A34dksf",
-      originalLink: "https://www.youtube.com/watch",
-      clicks: 784,
-      status: "deactive",
-    },
-  ];
+  const links = useSelector((state) => state.links);
+
   return (
-    <div className="mt-12 rounded-md overflow-hidden flex flex-col gap-1 md:w-[70%] mx-auto">
-      <div className="text-white capitalize bg-[#181E29] py-2 px-4 font-comfortaa text-sm font-600 grid grid-cols-[170px_1fr_80px_80px_80px_80px] items-center justify-between">
-        <p className="text-center">Short Link</p>
-        <p className="text-center">Original Link</p>
-        <p className="text-center">Qr code</p>
-        <p className="text-center">clicks</p>
-        <p className="text-center">status</p>
-        <p className="text-center">action</p>
-      </div>
+    <div className="mt-6 max-w-[32rem] mx-auto">
       <div className="flex flex-col gap-2">
         {links.map((link, index) => (
           <SingleLink key={index} link={link} />
@@ -60,38 +16,31 @@ const AllLinks = () => {
 };
 
 const SingleLink = ({ link }) => {
-  const { shortLink, originalLink, clicks, status } = link;
+  const { route, destinationUrl, clicks } = link;
   return (
-    <div className="text-[#C9CED6] bg-[#0E131E] grid py-2 px-4 font-comfortaa text-sm font-600  grid-cols-[170px_1fr_80px_80px_80px_80px] items-center justify-between">
-      <div className="flex items-center gap-1 justify-center">
-        <p className="">{shortLink}</p>
-        <button className="hover:bg-[#172234] w-8 h-8 grid place-content-center rounded-full">
+    <div className="bg-white flex items-center gap-8 z-10 relative py-2 px-2 outline outline-1 outline-gray-500/50 rounded">
+      <div className="flex items-center gap-2 ">
+        <div className="w-12 rounded-full overflow-hidden ">
+          <img src="http://robohash.org/2" alt="" />
+        </div>
+        <div>
+          <p className="font-bold">{`openurl.me/${route}`}</p>
+          <a className="hover:underline" href={destinationUrl}>
+            {destinationUrl}
+          </a>
+        </div>
+      </div>
+      <div className="flex items-center gap-4">
+        <button className="hover:bg-gray-200 rounded-full w-6 h-6 grid place-content-center p-1">
           <Clipboard className="w-5" />
         </button>
-      </div>
-      <p className="text-center overflow-hidden">
-        {originalLink.length > 20
-          ? originalLink.slice(0, 20) + "..."
-          : originalLink}
-      </p>
-      <div className="flex justify-center">
-        <button className="hover:bg-[#172234] rounded-md px-1 py-1">
-          <QrCode />
+        <button className="hover:bg-gray-200 rounded-full w-6 h-6 grid place-content-center p-1">
+          <QrCode className="w-5" />
         </button>
-      </div>
-      <p className="text-center">{clicks}</p>
-      <p
-        className={
-          status === "active"
-            ? "text-center text-green-600"
-            : "text-center text-red-600"
-        }>
-        {status}
-      </p>
-      <div className="flex justify-center">
-        <button className="hover:bg-[#172234] rounded-full w-7 h-7 flex justify-center items-center">
-          <Pencil className="w-4" />
-        </button>
+        <div className="flex font-bold bg-slate-200 gap-1 px-1 rounded">
+          <BarChart className="w-5" />
+          <p className="">{clicks}</p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Trial.jsx
+++ src/pages/Trial.jsx
@@ -1,5 +1,6 @@
 import Container from "../components/Container";
 import Cta from "../components/Cta";
+import AllLinks from "../components/AllLinks";
 const Trial = () => {
   return (
     <div className="overflow-hidden bg-gradient w-full relative min-h-screen bg-[#F8F9FA]">
@@ -9,6 +10,7 @@ const Trial = () => {
       <div className="pointer-events-none w-72 h-72 filter blur-2xl mix-blend-multiply rounded-full bg-[#FB5607] absolute animate-blob_2 animation-delay-4000 opacity-20 top-20 left-[28rem] lg:left-[40rem] "></div>
       <Container>
         <Cta />
+        <AllLinks />
       </Container>
     </div>
   );
