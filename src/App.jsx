027e63f1a896330e103765a84a0adf02b88d7875
adii1203 <aditya32ft@gmail.com>
2023-12-17 17:02:43 +0530

    Add new routes for registration and authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Trial from "./pages/Trial";
+import Register from "./pages/Register";
+import Emailsend from "./pages/auth/Emailsend";
+import Auth from "./pages/auth/Auth";
+import { ErrorFour } from "./pages/Error";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="*" element={<ErrorFour />} />
           <Route path="/" element={<Trial />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/user/auth" element={<Auth />}>
+            <Route path="verify/:id" element={<Emailsend />} />
+            <Route path="verify-email" element={<div></div>} />
+            <Route path="resetpassword" element={<div>reset password</div>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
