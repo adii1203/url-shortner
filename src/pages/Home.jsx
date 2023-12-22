dcf1cd821fca0599f4dbe450a44ced967374ff2c
adii1203 <aditya32ft@gmail.com>
2023-12-22 22:39:36 +0530

    Add user to Nav component and set document title in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Nav from "../components/dashboard/Nav";
 import Container from "../components/Container";
 import Dashboard from "../components/dashboard/Dashboard";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 const Home = () => {
+  const { user } = useSelector((state) => state.user);
+  useEffect(() => {
+    document.title = "Dashboard";
+  }, []);
   return (
     <div className="bg-[#f9fafb] min-h-screen  mx-auto ">
       <Container>
-        <Nav />
+        <Nav user={user} />
         <div className="mt-5 ">
           <Dashboard />
         </div>
