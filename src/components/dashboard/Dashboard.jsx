643819737ae81fd7d2975ac8287e6b3790cb0c6f
adii1203 <aditya32ft@gmail.com>
2023-12-28 15:47:42 +0530

    Refactor Dashboard and Links components

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,18 +1,23 @@
 /* eslint-disable react/prop-types */
 import SearchBtn from "./SearchBtn";
 import Links from "./Links";
-import { useState } from "react";
-import CreateLink from "../CreatLink";
 import Button from "../ui/Button";
-const Dashboard = ({ links }) => {
-  const [isCreateLinkModelOpen, setIsCreateLinkModelOpen] = useState(false);
+import { useShowLinkOption } from "../../hooks/useShowLinkOption";
+import { useCreatEditModel } from "../../hooks/useCreatEditModel";
 
+const Dashboard = ({ links }) => {
+  const { showLinkOption, handleShowLinkOption } = useShowLinkOption();
+  const {
+    CreatEditLinkModelCallback,
+    setShowCreateEditModel,
+    showCreateEditModel,
+  } = useCreatEditModel();
   return (
-    <div className=" h-screen">
+    <div className="  h-screen">
       <div className="flex  flex-col-reverse items-center justify-center gap-3 md:flex-row">
         <SearchBtn />
         <Button
-          onClick={() => setIsCreateLinkModelOpen(true)}
+          onClick={() => setShowCreateEditModel(true)}
           variant={"primary"}
           className={
             "text-white font-comfortaa font-500 w-full sm:w-1/2 transition-colors duration-300"
@@ -21,11 +26,16 @@ const Dashboard = ({ links }) => {
         </Button>
       </div>
       {links.map((link) => {
-        return <Links key={link._id} link={link} />;
+        return (
+          <Links
+            showLinkOption={showLinkOption}
+            handleShowLinkOption={handleShowLinkOption}
+            key={link._id}
+            link={link}
+          />
+        );
       })}
-      {isCreateLinkModelOpen && (
-        <CreateLink setIsCreateLinkModelOpen={setIsCreateLinkModelOpen} />
-      )}
+      {showCreateEditModel && <CreatEditLinkModelCallback />}
     </div>
   );
 };
--- src/components/dashboard/Links.jsx
+++ src/components/dashboard/Links.jsx
@@ -1,40 +1,61 @@
 /* eslint-disable react/prop-types */
 import { MoreVertical } from "lucide-react";
+import LinkOptions from "./LinkOptions";
+import { useCreatEditModel } from "../../hooks/useCreatEditModel";
 
-const Links = ({ link }) => {
+const Links = ({ showLinkOption, handleShowLinkOption, link }) => {
+  const {
+    setShowCreateEditModel,
+    showCreateEditModel,
+    CreatEditLinkModelCallback,
+  } = useCreatEditModel(link);
+
+  const handelEditLink = () => {
+    handleShowLinkOption(link._id);
+    setShowCreateEditModel(true);
+  };
+
   return (
-    <div className="w-full mt-5 md:w-[30rem] lg:w-[42rem] mx-auto ">
-      <div className="bg-white rounded-md px-2 py-3 shadow-md hover:scale-[1.01] transition-transform duration-200 flex items-center justify-between">
-        <div className="flex items-center gap-4">
-          <div className="w-8 border rounded-full overflow-hidden">
-            <img src={link.icon} alt="" />
+    <>
+      <div className="w-full relative mt-5 md:w-[30rem] lg:w-[42rem] mx-auto ">
+        <div className="bg-white rounded-md px-2 py-3 shadow-md hover:scale-[1.01] transition-transform duration-200 flex items-center justify-between">
+          <div className="flex items-center gap-4">
+            <div className="w-8 border rounded-full overflow-hidden">
+              <img src={link.icon} alt="" />
+            </div>
+            <div className="font-Outfit">
+              <a
+                href="https://openurl.me/af46Fg"
+                target="_blank"
+                rel="noreferrer"
+                className="block text-blue-700 text-lg">
+                {`openurl.me/${link.key}`}
+              </a>
+              <a
+                href={link.originUrl}
+                target="_blank"
+                rel="noreferrer"
+                className=" font-comfortaa text-sm underline">
+                {link.originUrl?.length > 35
+                  ? link.originUrl.slice(0, 35) + "..."
+                  : link.originUrl}
+              </a>
+            </div>
           </div>
-          <div className="font-Outfit">
-            <a
-              href="https://openurl.me/af46Fg"
-              target="_blank"
-              rel="noreferrer"
-              className="block text-blue-700 text-lg">
-              {`openurl.me/${link.key}`}
-            </a>
-            <a
-              href={link.originUrl}
-              target="_blank"
-              rel="noreferrer"
-              className=" font-comfortaa text-sm underline">
-              {link.originUrl?.length > 35
-                ? link.originUrl.slice(0, 35) + "..."
-                : link.originUrl}
-            </a>
+          <div>
+            <button
+              onClick={() => handleShowLinkOption(link._id)}
+              className="w-4 grid place-content-center rounded-sm hover:bg-slate-500/20 transition-colors duration-200">
+              <MoreVertical />
+            </button>
           </div>
         </div>
-        <div>
-          <button className="w-4 grid place-content-center rounded-sm hover:bg-slate-500/20 transition-colors duration-200">
-            <MoreVertical />
-          </button>
-        </div>
+        {showLinkOption?.includes(link._id) && (
+          <LinkOptions handelEditLink={handelEditLink} />
+        )}
       </div>
-    </div>
+      {showCreateEditModel && <CreatEditLinkModelCallback />}
+    </>
   );
 };
 
