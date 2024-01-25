a50282892719ac33b2e81fdf250e430bfff29d8f
adii1203 <aditya32ft@gmail.com>
2024-01-25 23:37:14 +0530

    Revert "Update API base URL"
    This reverts commit 4a15454ef2c45717116db1db8a6dcfe01cc62dc4.

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { refrechCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://api.openurl.me/api/v1",
+  baseUrl: "https://openurl.me/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.authToken;
